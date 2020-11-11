5793fd6f7be1371e8e5e105d96b439c3d8a3ae85
PomidorS <14v20slv16@mail.com>
2020-11-11 21:32:17 +0700

    PushCod

--- app/Filter/ListsFilter.php
+++ app/Filter/ListsFilter.php
@@ -1,1 +1,24 @@
 <?php
+namespace App\Filter;
+
+
+class ListsFilter extends QueryFilter
+{
+
+    protected function name($value)
+    {
+        $this->create = $this->create->where('name', $value);
+    }
+
+    protected function created_at($value)
+    {
+        $this->create = $this->create->where('created_at', $value);
+    }
+
+    protected function updated_at($value)
+    {
+        $this->create = $this->create->where('updated_at', $value);
+    }
+
+}
+
