6a0dd016fc86ceb411b98da3ae0917cc43664ef9
PomidorS <14v20slv16@mail.com>
2020-11-11 21:32:54 +0700

    PushCod

--- app/Filter/QueryFilter.php
+++ app/Filter/QueryFilter.php
@@ -1,1 +1,33 @@
 <?php
+namespace App\Filter;
+
+
+class QueryFilter
+{
+    protected $create;
+    protected $req;
+
+    public function __construct($create, $req)
+    {
+        $this->create = $create;
+        $this->req = $req;
+    }
+
+    public function apply()
+    {
+        foreach ($this->filters() as $filter => $value) {
+            if (method_exists($this, $filter)) {
+                if (!$value) {
+                    continue;
+                }
+                $this->$filter($value);
+            }
+        }
+        return $this->create;
+    }
+
+    protected function filters()
+    {
+        return $this->req->all();
+    }
+}
--- app/Filter/TasksFilter.php
+++ app/Filter/TasksFilter.php
@@ -1,1 +1,26 @@
 <?php
+namespace App\Filter;
+
+
+class TasksFilter extends QueryFilter
+{
+    protected function name($value)
+    {
+        $this->create = $this->create->where('name', $value);
+    }
+
+    protected function done($value)
+    {
+        $this->create = $this->create->where('done', $value);
+    }
+
+    protected function created_at($value)
+    {
+        $this->create = $this->create->where('created_at', $value);
+    }
+
+    protected function updated_at($value)
+    {
+        $this->create = $this->create->where('updated_at', $value);
+    }
+}
